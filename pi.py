399a98d4ab345678629fd60809020c694fea2a0b
Tony <extracat@users.noreply.github.com>
2023-12-26 00:04:31 +0100

    save

--- pi.py
+++ pi.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
-file_path = './data/pi_dec_1m.txt'
+file_path = './data/pi_dec_10b.txt'
 
 @app.route('/find_position', methods=['GET'])
 def find_position_api():
